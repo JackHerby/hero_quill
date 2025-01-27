f22aa4772b995a636ad2ee9b256d3ac369456602
Jack Herby <JacekZiolek@proton.me>
2025-01-27 21:19:49 +0100

    Configure Supabase client

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
+<script setup lang="ts">
+import { createClient } from "@supabase/supabase-js";
+
+const config = useRuntimeConfig();
+const supabase = createClient(config.public.supabaseUrl, config.public.supabaseKey);
+const testData = ref();
+
+async function getTestData() {
+  const { data } = await supabase.from("test").select();
+  testData.value = data;
+}
+
+onMounted(() => getTestData());
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 v-for="item in testData" :key="item.id">{{ item.name }} -- {{ item.created_at }}</h1>
   </div>
 </template>
