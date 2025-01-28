ec516d00c5c4b448db944868f41e48e80932e3d9
Jack Herby <JacekZiolek@proton.me>
2025-01-28 20:58:11 +0100

    Update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
-    modules: ["@nuxt/ui"],
+    modules: ["@nuxt/ui", "@nuxt/test-utils/module"],
     runtimeConfig: {
         public: {
             supabaseUrl: process.env.SUPABASE_URL,
