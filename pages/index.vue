48fafa179c605f013a1a40c61fe1e8ef1ea47de7
Jack Herby <JacekZiolek@proton.me>
2025-03-13 23:16:04 +0100

    Auth WIP

--- pages/index.vue
+++ pages/index.vue
@@ -11,34 +11,11 @@ getTestData();
 
 const { data: _data } = useDndApiData('ability-scores/cha');
 
-const user = ref();
-async function getUser() {
-  console.log('getUser');
-  try {
-    const data = await $fetch('/api/me');
-    user.value = data;
-  } catch (error) {
-    console.log('isNuxtError: ', isNuxtError(error));
-    showError({
-      statusCode: error.statusCode,
-      statusMessage: error.statusMessage,
-    });
-    // if (isNuxtError(error)) {
-    //  showError({
-    //    statusCode: error.statusCode,
-    //    statusMessage: error.statusMessage,
-    //  });
-    // } else {
-    //  showError({
-    //    statusCode: 404,
-    //    statusMessage: 'Something went wrong',
-    //  });
-    // }
-  }
-}
+const { data: user, error, refresh } = await useFetch('/api/me');
 
 async function signOut() {
   await $fetch('/api/signout', { method: 'POST' });
+  refresh();
 }
 </script>
 
@@ -58,15 +35,10 @@ async function signOut() {
     </ULink>
   </nav>
   <UContainer>
-    <h1 v-for="item in testData" :key="item.id">
-      {{ item.name }} -- {{ item.created_at }}
-    </h1>
-    <UButton @click="getUser">
-      Get User
-    </UButton>
     <UButton @click="signOut">
       Sign Out
     </UButton>
     <pre>{{ user }}</pre>
+    <pre>{{ error }}</pre>
   </UContainer>
 </template>
